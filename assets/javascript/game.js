b95691881b48b5b1ef49193027c408b8b38b794e
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2018-10-20 23:57:02 -0500

    updating files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var winsValue = 0;
 var lossesValue = 0;
 var lastGuessValue = "";
 var alreadyGuessedValue = [];
+var alreadyGuessedValuePrevious = [];
 var userInningsRemaining = document.getElementById("inningsRemaining");
 var userLastGuess = document.getElementById("lastGuess");
 var userAlreadyGuessed = document.getElementById("alreadyGuessed");
@@ -15,6 +16,7 @@ var wordsListLength = wordsList.length;
 
 var main = [];
 var mainWordArray = [];
+var mainWordArrayPrevious = [];
 var mainWordUnderlined = [];
 
 var mainWord = "";
@@ -29,54 +31,87 @@ function pullMainWord() {
 
 
 function setUnderline() {
+    mainWordArrayPrevious = mainWordArray;
+    mainWordArray = [];
+    alreadyGuessedValue = [];
+    inningsValue = 9;
     pullMainWord();
     for (i = 0; i < mainWord.length; i++) {
         mainWordArray.push("_");
     }
     mainWordU = mainWordArray.join(" ");
     document.getElementById("wordToPlay").textContent = mainWordU;
+    userInningsRemaining.textContent = "Innings Remaining: 9";
+    userLastGuess.textContent = "Last Guess: ";
+    userAlreadyGuessed.textContent = "Already Guessed: ";
+    document.getElementById("playButton").disabled = "true";
 }
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
-    var mainChanges = 0;
-    var mainChangesTwo = 0;
-    alert("You have selected: " + userGuess)
-    for (i = 0; i < mainWord.length; i++) {
-        lastGuess = 0;
-        if (userGuess == mainWord[i]) {
-            mainWordArray[i] == userGuess;
-            mainWordArray.splice(i, 1, userGuess);
-            mainChanges += 1;
+    if (userGuess === "a" || userGuess === "b" || userGuess === "c" || userGuess === "d" || userGuess === "e" || userGuess === "f" || userGuess === "g" || userGuess === "h" || userGuess === "i" || userGuess === "j" || userGuess === "k" || userGuess === "l" || userGuess === "m" || userGuess === "n" || userGuess === "o" || userGuess === "p" || userGuess === "q" || userGuess === "r" || userGuess === "s" || userGuess === "t" || userGuess === "u" || userGuess === "v" || userGuess === "w" || userGuess === "x" || userGuess === "y" || userGuess === "z"){
+        var mainChanges = 0;
+        var mainChangesTwo = 0;
+        alert("You have selected: " + userGuess)
+        for (i = 0; i < mainWord.length; i++) {
+            lastGuess = 0;
+            if (userGuess == mainWord[i]) {
+                mainWordArray[i] == userGuess;
+                mainWordArray.splice(i, 1, userGuess);
+                mainChanges += 1;
+            }
         }
-    }
-    if (mainChanges === mainChangesTwo) {
-        inningsValue -= 1;
-        console.log(mainChanges)
-        console.log(mainChangesTwo)
-    }
-    alreadyGuessedValue.push(userGuess);
-    userAlreadyGuessed.textContent = "Already Guessed: " + alreadyGuessedValue;
-    userInningsRemaining.textContent = "Innings Remaining: " + inningsValue;
-    userLastGuess.textContent = "Last Guess: " + userGuess;
-    mainWordU = mainWordArray.join(" ");
-    document.getElementById("wordToPlay").textContent = mainWordU;
+        if (mainChanges === mainChangesTwo) {
+            inningsValue -= 1;
+            console.log(mainChanges)
+            console.log(mainChangesTwo)
+        }
+        alreadyGuessedValue.push(userGuess);
+        userAlreadyGuessed.textContent = "Already Guessed: " + alreadyGuessedValue;
+        userInningsRemaining.textContent = "Innings Remaining: " + inningsValue;
+        userLastGuess.textContent = "Last Guess: " + userGuess;
+        mainWordU = mainWordArray.join(" ");
+        document.getElementById("wordToPlay").textContent = mainWordU;
 
-    if (inningsValue == 0) {
-        alert("That's it folks! Click button to restart!");
-        lossesValue++;
-        document.getElementById("losses").textContent = "Losses: " + lossesValue;
+        if (inningsValue == 0) {
+            alert("That's unfortunate, you lost! Click the 'Click to Play' button to Play again!");
+            lossesValue++;
+            document.getElementById("losses").textContent = "Losses: " + lossesValue;
+            mainWordArray.splice(0, 100, "");
+            userInningsRemaining.textContent = "Innings Remaining: 9";
+            userLastGuess.textContent = "Last Guess: ";
+            userAlreadyGuessed.textContent = "Already Guessed: ";
+            // document.getElementById("playButton").disabled = "false";
+            setUnderline();
+
+
+        }
+
+
+
+        var mainWordArrayTwo = mainWordArray.join("")
+
+        if (mainWordArrayTwo === mainWord) {
+            alert("You won! Click the 'Click to Play' button to Play again!")
+            winsValue++;
+            document.getElementById("wins").textContent = "Wins: " + winsValue;
+            mainWordArray.splice(0, 100, "");
+            userInningsRemaining.textContent = "Innings Remaining: 9";
+            userLastGuess.textContent = "Last Guess: ";
+            userAlreadyGuessed.textContent = "Already Guessed: ";
+            // document.getElementById("playButton").disabled = "false";
+            setUnderline();
+
+        }
+
     }
-
-    
-
-    var mainWordArrayTwo = mainWordArray.join("")
-    
-    if (mainWordArrayTwo === mainWord) {
-        alert("You won!")
-        winsValue++;
-        document.getElementById("wins").textContent = "Wins: " + winsValue;
-    }
-
+    document.getElementById("playButton").disabled = "false";
 }
+
+document.getElementById("playButton").onclick = function () { setUnderline() };
+
+
+
+
+
 
